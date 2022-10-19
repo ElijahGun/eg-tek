6ec6b3bc22b7357aa248f80ceea6fa1270947b48
Elijah Gundorin <elijahgundorin@Elijahs-MacBook-Pro.local>
2022-10-19 14:23:38 -0400

    added firebase google auth

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+import { Root } from './root';
+import ProductHero from './components/product-hero/product-hero';
+import { ErrorPage } from './components/error-page/error-page';
+import Laptops from './routes/laptops/laptops';
+import SignIn from './components/sign-in/sign-in';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <ProductHero />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: 'shop',
+        element: <div>this is the shop page</div>
+      },
+      {
+        path: 'sign-in',
+        element: <SignIn />
+      },
+      {
+        path: 'cart',
+        element: <h1>buy everything!</h1>
+      },
+      {
+        path: 'laptops',
+        element: <Laptops />,
+        children: [
+          {
+            path: 'macbook',
+            element: <h1>macbook all the way!</h1>
+          }
+        ]
+      }
+    ]
+
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
